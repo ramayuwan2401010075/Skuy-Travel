ebf618b7698023ad44aa4bca71850a8d6c481dea
ramayuwan099 <ramayuwan099@gmail.com>
2024-11-20 13:32:49 +0000

    commit

--- main.js
+++ main.js
@@ -1,1 +1,25 @@
-// Add JS here
+let slideIndex = 0;
+
+function showSlides() {
+    let slides = document.getElementsByClassName("slide");
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display="none";
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) { slideIndex = 1; }
+    slides[slideIndex - 1].style.display="block";
+    setTimeout(showSlides, 3000); 
+}
+function changeSlide(n) {
+    let slides = document.getElementsByClassName("slide");
+    slideIndex += n;
+    if (slideIndex > slides.length) { slideIndex = 1; }
+    if (slideIndex < 1) { slideIndex = slides.length; }
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slides[slideIndex - 1].style.display = "block";
+}
+
+
+document.addEventListener("DOMContentLoaded", showSlides);
